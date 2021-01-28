bf8e0cd3fb6104c3bd9140aed42047ba78fdea9f
Spenser Black <spenserblack01@gmail.com>
2021-01-28 10:30:03 -0500

    Implement ParseDie

--- pkg/dice/die.go
+++ pkg/dice/die.go
@@ -1,9 +1,15 @@
 package dice
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 // Die represents a single die (e.g. a d6).
 type Die int
+
+var dieRegex = regexp.MustCompile(`^\s*d(\d+)\s*$`)
 
 // Roll returns a random value by rolling the die. Will always be between 1
 // and the die value, inclusively.
@@ -17,5 +23,15 @@ func (d Die) String() string {
 
 // ParseDie parses a string in the format "dX" into a Rollable type.
 func ParseDie(notation string) (d Die, err error) {
+	matches := dieRegex.FindStringSubmatch(notation)
+
+	if len(matches) != 2 {
+		err = fmt.Errorf("parsing %q: invalid syntax", notation)
+	} else {
+		var n int
+		n, err = strconv.Atoi(matches[1])
+		d = Die(n)
+	}
+
 	return
 }
