745399906cf51a691c448c9628cdcb890b792472
Spenser Black <spenserblack01@gmail.com>
2021-01-29 15:39:26 -0500

    Add diceroll cmd

---
+++ cmd/diceroll/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spenserblack/diceroll/pkg/dice"
+	"os"
+	"strings"
+)
+
+var allDice = strings.Join(os.Args[1:], "")
+
+func main() {
+	var rollable dice.Rollable
+	var err error
+
+	if rollable, err = dice.ParseDie(allDice); err == nil {
+	} else if rollable, err = dice.ParseSet(allDice); err == nil {
+	} else if rollable, err = dice.ParseCombo(allDice); err == nil {
+	} else {
+		panic(fmt.Errorf("Couldn't parse to a rollable type: %q", allDice))
+	}
+
+	fmt.Println(rollable.Roll())
+}
