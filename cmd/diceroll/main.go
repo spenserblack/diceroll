4858dca204120579066c4e5557fecfee2e8b9184
Spenser Black <spenserblack01@gmail.com>
2021-02-01 09:28:17 -0500

    Print usage when no args or "help" is passed

--- cmd/diceroll/main.go
+++ cmd/diceroll/main.go
@@ -9,12 +9,16 @@ import (
 	"time"
 )
 
-var allDice = strings.Join(os.Args[1:], "")
-
 func main() {
+	if len(os.Args[1:]) == 0 || os.Args[1] == "help" {
+		println("Usage: diceroll <dice notation>")
+		return
+	}
+
 	var rollable dice.Rollable
 	var err error
 
+	allDice := strings.Join(os.Args[1:], "")
 	if rollable, err = dice.ParseDie(allDice); err == nil {
 	} else if rollable, err = dice.ParseSet(allDice); err == nil {
 	} else if rollable, err = dice.ParseCombo(allDice); err == nil {
